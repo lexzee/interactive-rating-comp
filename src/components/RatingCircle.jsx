c1de6a299f734d60da1990027f4b8b85f1ec60fa
M.Kristof <drzero123456@gmail.com>
2023-06-10 20:26:41 +0200

    Separating the rating button

---
+++ src/components/RatingCircle.jsx
@@ -0,0 +1,21 @@
+
+export const RatingCircle = ({rate,value,handleClick}) => {
+
+    const {id,style} = rate
+
+    const btnStyle = {backgroundColor: 'blue'}
+
+    const handleHover = (e) => {
+        e.target.classList.toggle("hovered")
+    }
+    return(
+        <button
+        className="rate"
+        id={value}
+        onClick={handleClick}
+        onMouseOver= {handleHover}
+        onMouseLeave = {handleHover}
+        style = {{ backgroundColor: +id === value  ? "red" : "",}}
+      >{value}</button>
+    )
+}
